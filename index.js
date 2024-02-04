eae9c52e28a679ca7200d07c1e0469bc39831b8c
azelyazel <158849140+azelyazel@users.noreply.github.com>
2024-02-04 23:52:24 +0500

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["Hard Work","Join HardenFamily"];
 
 
 let currentIndex = 0;
